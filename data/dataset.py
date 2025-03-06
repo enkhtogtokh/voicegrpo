65a095adca726ebcab5ddea393541398110f271a
Enkhtogtokh <enkhtogtokh.java@gmail.com>
2025-03-06 11:00:03 +0100

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -7,10 +7,28 @@ from torch.utils.data import  TensorDataset
 
 class Dataset:
     def __init__(self, dataset_path):
+        """
+        Constructor for the Dataset class.
+
+        Parameters
+        ----------
+        dataset_path : str
+            The path to the dataset Excel file.
+        """
         self.dataset_path = dataset_path
 
     def prepare(self):
         
+        """
+        Prepares the dataset for use.
+
+        Returns
+        -------
+        train_dataset : TensorDataset
+            The training dataset.
+        test_dataset : TensorDataset
+            The testing dataset.
+        """
         df = pd.read_excel(self.dataset_path)
         features, labels = df[['pitch', 'jitter', 'shimmer', 'hnr', 'age', 'disease_severity']], df['pathology']
  
